d6b9e5001b606e912edfb43b382277ce0783bea1
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-04-14 12:11:46 +0200

    logical refactor

--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Cocktail;
 use App\Form\BotType;
+use Symfony\Component\Form\Exception\LogicException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -19,10 +20,28 @@ class CocktailController extends Controller
     public function index(Request $request, SessionInterface $session)
     {
         $session->start();
+        $chatMessages = [];
+        $userMessages = [];
 
-        //on teste si on est au premier tour
-        if ($session->get('cocktails') == null || $session->get('ingredients') == null) {
-            //si oui on initialise nos données
+        //si on est en POST
+        if ($request->request->get('bot') !== null) {
+
+            //on récupère nos données
+            $cocktails = $session->get('cocktails');
+            $ingredients = $session->get('ingredients');
+
+            //clic sur oui
+            if (isset($request->request->get('bot')['yes'])){
+                $session = $this->clicYes($session);
+            }
+            //clic sur non
+            if (isset($request->request->get('bot')['no'])){
+                $session = $this->clicNo($session);
+            }
+        } else {
+            //si on est en GET c'est le premier tour
+
+            //initialisation des données
             $string = file_get_contents("../public/api/cocktail.json");
             $json = json_decode($string, true);
             $cocktails = [];
@@ -38,23 +57,6 @@ class CocktailController extends Controller
             $session->set('cocktails', $cocktails);
             $session->set('ingredients', $ingredients);
 
-
-            $session->set('round', 0);
-
-        } else {
-            //sinon on récupère nos données
-            $cocktails = $session->get('cocktails');
-            $ingredients = $session->get('ingredients');
-
-            $round = $session->get('round');
-            $round++;
-            $session->set('round', $round);
-        }
-
-        $chatMessages = [];
-        $userMessages = [];
-        if ($session->get('round') == 0) {
-            $session->set('round', 1);
             //lancement de la conversation
             $chatMessages[] = "Bonjour je suis Drinky.";
             $chatMessages[] = "Je vais vous proposer un cocktail.";
@@ -67,62 +69,15 @@ class CocktailController extends Controller
             $chatMessages[] = "L'ingrédient suivant vous convient-il ?";
             $chatMessages[] = $ingredientSelected;
 
-        } else {
-            //récupérer le post si oui on ne garde que les cocktails avec l'ingrédient sinon on relance un autre tour
-            if ($request->request->get('bot') !== null) {
-                if (isset($request->request->get('bot')['yes'])) {
-                    $userMessages[] = "Oui";
-                    $cocktails = $session->get('cocktails');
-                    $cocktails = $this->filterCocktailsByIngredient($session->get('ingredientSelected'), $cocktails);
-                    $session->set('cocktails', $cocktails);
-                    //on teste si il reste plus de 2 cocktails
-                    if (count($session->get('cocktails')) < 2) {
-                        if (count($session->get('cocktails')) < 1) {
-                            $chatMessages[] = "Nous n'avons pas de cocktail à votre gout.";
-                        } else {
-                            $chatMessages[] = "Il reste un seul cocktail.";
-                            $lastCocktail = array_shift($cocktails);
-                            $chatMessages[] = 'Je vous propose le cocktail :';
-                            $chatMessages[] = $lastCocktail->getName();
-                        }
-
-                    } else {
-                        $chatMessages[] = "Je vais vous faire une autre proposition.";
-                        //selection d'un nouvel ingrédient présent dans les cocktails restants
-                        $results = $this->selectOneIngredient($session->get('ingredients'));
-                        $ingredientSelected = $results[0];
-                        $session->set('ingredientSelected', $ingredientSelected);
-                        $session->set('ingredients', $results[1]);
-                        dump($session);
-                        while (!$this->testIngredientInCocktails($ingredientSelected, $session->get('cocktails'))) {
-                            $results = $this->selectOneIngredient($session->get('ingredients'));
-                            $ingredientSelected = $results[0];
-                            $session->set('ingredientSelected', $ingredientSelected);
-                            $session->set('ingredients', $results[1]);
-                        }
-                        dump($session);
-                        $session->set('ingredientSelected', $ingredientSelected);
-
-                        $chatMessages[] = "Je vous propose :";
-                        $chatMessages[] = $ingredientSelected;
-
-                    }
-                } else {
-                    $userMessages[] = "Non";
-
-                    $array = $this->selectOneIngredient($ingredients);
-                    $ingredientSelected = $array[0];
-                    $session->set('ingredientSelected', $ingredientSelected);
-                    $ingredients = $array[1];
-                    $session->set('ingredients', $ingredients);
-                    $chatMessages[] = "L'ingrédient suivant vous convient-il ?";
-                    $chatMessages[] = $ingredientSelected;
-                }
-
-            }
-
-            $chatMessages[] = '';
+            $session->set('chatMessages', $chatMessages);
         }
+
+
+
+
+
+
+
 
 
         $form = $this->createForm(BotType::class);
@@ -138,8 +93,8 @@ class CocktailController extends Controller
         return $this->render('cocktail/index.html.twig', [
             'cocktails' => $cocktails,
             'ingredients' => $ingredients,
-            'chatMessages' => $chatMessages,
-            'userMessages' => $userMessages,
+            'chatMessages' => $session->get('chatMessages'),
+            'userMessages' => $session->get('$userMessages'),
             'form' => $form->createView()
         ]);
     }
@@ -154,6 +109,11 @@ class CocktailController extends Controller
         return $this->redirectToRoute("cocktail");
     }
 
+    /**
+     * @param int $id
+     * @param array $cocktails
+     * @return Cocktail
+     */
     public function selectOneCocktail(int $id, array $cocktails): Cocktail
     {
         foreach ($cocktails as $cocktail) {
@@ -171,12 +131,20 @@ class CocktailController extends Controller
      */
     public function selectOneIngredient(array $ingredients): array
     {
+        if (count($ingredients) < 1) {
+            throw new LogicException('Il ne reste plus d\'élément dans le tableau.');
+        }
         $index = array_rand($ingredients, 1);
         $ingredient = $ingredients[$index];
         array_splice($ingredients, $index, 1);
         return [$ingredient, $ingredients];
     }
 
+    /**
+     * @param string $ingredientSelected
+     * @param array $cocktails
+     * @return array
+     */
     public function filterCocktailsByIngredient(string $ingredientSelected, array $cocktails): array
     {
         $cocktailsSelected = [];
@@ -188,6 +156,11 @@ class CocktailController extends Controller
         return $cocktailsSelected;
     }
 
+    /**
+     * @param string $ingredient
+     * @param array $cocktails
+     * @return bool
+     */
     public function testIngredientInCocktails(string $ingredient, array $cocktails): bool
     {
         foreach ($cocktails as $cocktail) {
@@ -196,6 +169,78 @@ class CocktailController extends Controller
             }
         }
         return false;
+    }
+
+    public function clicYes($session)
+    {
+        dump($session);
+
+        $userMessages[] = "Oui";
+        $cocktails = $session->get('cocktails');
+        dump($cocktails);
+        $cocktails = $this->filterCocktailsByIngredient($session->get('ingredientSelected'), $cocktails);
+        $session->set('cocktails', $cocktails);
+
+        if (count($session->get('cocktails')) < 1) {
+            $chatMessages[] = "Nous n'avons pas de cocktail à votre gout.";
+        } else if (count($session->get('cocktails')) == 1) {
+
+            $lastCocktail = array_shift($cocktails);
+            $chatMessages[] = 'Je vous propose le cocktail :';
+            $chatMessages[] = $lastCocktail->getName();
+        } else {
+            $chatMessages[] = "Nous avons encore du choix.";
+            $chatMessages[] = "Je vais vous faire une autre proposition.";
+            //selection d'un nouvel ingrédient présent dans les cocktails restants
+            $results = $this->selectOneIngredient($session->get('ingredients'));
+            $ingredientSelected = $results[0];
+            $session->set('ingredientSelected', $ingredientSelected);
+            $session->set('ingredients', $results[1]);
+
+            while (!$this->testIngredientInCocktails($ingredientSelected, $session->get('cocktails'))) {
+                $results = $this->selectOneIngredient($session->get('ingredients'));
+                $ingredientSelected = $results[0];
+                $session->set('ingredientSelected', $ingredientSelected);
+                $session->set('ingredients', $results[1]);
+            }
+            $session->set('ingredientSelected', $ingredientSelected);
+
+            $chatMessages[] = "Je vous propose :";
+            $chatMessages[] = $ingredientSelected;
+        }
+
+        $session->set('chatMessages', $chatMessages);
+        $session->set('userMessages', $userMessages);
+
+        return $session;
+    }
+
+    public function clicNo($session)
+    {
+        $userMessages[] = "Non";
+
+        $array = $this->selectOneIngredient($session->get('ingredients'));
+        $ingredientSelected = $array[0];
+        $session->set('ingredientSelected', $ingredientSelected);
+        $ingredients = $array[1];
+        $session->set('ingredients', $ingredients);
+
+        while (!$this->testIngredientInCocktails($ingredientSelected, $session->get('cocktails'))) {
+            $results = $this->selectOneIngredient($session->get('ingredients'));
+            $ingredientSelected = $results[0];
+            $session->set('ingredientSelected', $ingredientSelected);
+            $session->set('ingredients', $results[1]);
+        }
+        $session->set('ingredientSelected', $ingredientSelected);
+
+
+        $chatMessages[] = "L'ingrédient suivant vous convient-il ?";
+        $chatMessages[] = $ingredientSelected;
+
+        $session->set('chatMessages', $chatMessages);
+        $session->set('userMessages', $userMessages);
+
+        return $session;
     }
 }
 
