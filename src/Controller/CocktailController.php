2e8faf74e8eab3273460f45d259a64ef95e7f156
Julien <julien.alexandre26@gmail.com>
2018-04-14 02:22:27 +0200

    Création des objets Cocktail et envoie à la vue.

--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Cocktail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,9 +14,14 @@ class CocktailController extends Controller
      */
     public function index(Request $request)
     {
+        $string = file_get_contents("../public/api/cocktail.json");
+        $json = json_decode($string, true);
+        $cocktails = [];
 
+        foreach ($json as $cocktail) {
+            $cocktails[] = new Cocktail($cocktail);
+        }
 
-
-        return $this->render('cocktail/index.html.twig');
+        return $this->render('cocktail/index.html.twig', ['cocktails' => $cocktails]);
     }
 }
